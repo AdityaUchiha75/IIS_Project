c20f0e81705e91a193971273d0eee15681a13fb5
unknown <nekalampokas@gmail.com>
2024-01-09 19:17:09 +0100

    Trying to make parallel processing work

--- furhat/excercises.py
+++ furhat/excercises.py
@@ -1,19 +1,19 @@
-
 from time import sleep
-from furhatConfig import LOOK_DOWN, LOOK_BACK
+from .furhatConfig import LOOK_DOWN, LOOK_BACK
+
 
 # ----- Visualization Excercise  -----
-def visualizationExercise(furhat):
-
+def visualizationExercise(furhat, emotion):
     # Visualization (1 minute)
     furhat.say(text="Now, let's enter a moment of visualization.")
     sleep(12)
-    furhat.say(text="Picture a serene place in your mind. It could be a beach, a forest, or any place that brings you peace.")
+    furhat.say(text="Picture a serene place in your mind. It could be a beach, a forest, or any place that brings you "
+                    "peace.")
     sleep(12)
     furhat.say(text="Imagine the sights, sounds, and smells of this tranquil setting.")
 
     # Add pauses to allow time for the user to visualize
-    sleep(12) 
+    sleep(12)
 
     furhat.say(text="With each breath, feel yourself absorbing the calm and positive energy from this place.")
     sleep(8)
@@ -23,13 +23,15 @@ def visualizationExercise(furhat):
     sleep(12)
 
     # Call the function that returns the user's current emotion
-    furhat.say("How are you feeling right now?") # angry, sad, calm, happy
-    emotion = furhat.listen().lower()
-    sleep(3)
+    # furhat.say("How are you feeling right now?")  # angry, sad, calm, happy
+    # emotion = furhat.listen().lower()
+    # sleep(3)
 
     # Call the function that returns the response to the user's current emotion
-    response = visualizationResponse(emotion)
-    furhat.say(f"I understand you're feeling {emotion}. Embrace visualization, and let it guide you towards a {response} state.")
+    visualizationResponse(furhat, emotion)
+    # furhat.say(
+    #     f"I understand you're feeling {emotion}. Embrace visualization, and let it guide you towards a {response} state.")
+
 
 # ----- Gratitude Excercise  -----
 def gratitudeExercise(furhat):
@@ -42,77 +44,101 @@ def gratitudeExercise(furhat):
     furhat.say(text="As you reflect on each one, feel a sense of gratitude and warmth.")
 
     # Add pauses to allow time for the user to experience the gratitude
-    sleep(12) 
+    sleep(12)
 
     # Prompt the user to share their current emotion
-    furhat.say("How are you feeling right now?") # (angry, sad, calm, happy, something)
+    furhat.say("How are you feeling right now?")  # (angry, sad, calm, happy, something)
     emotion = furhat.listen().lower()
     sleep(3)
 
-     # Call the custom emotion response function
+    # Call the custom emotion response function
     response = gratitudeEmotionResponse(emotion)
-    furhat.say(f"I understand you're feeling {emotion}. Embrace gratitude, and let it guide you towards a {response} state.")
+    furhat.say(
+        f"I understand you're feeling {emotion}. Embrace gratitude, and let it guide you towards a {response} state.")
+
 
 # ----- Visualization : Responses to each of the emotions : angry, happy, sad, calm/relaxed  -----
 def visualizationResponse(furhat, emotion):
     if emotion == "angry":
-        furhat.say("I see that you're angry. While in this visualization, acknowledge your anger and visualize letting it go. Picture your serene place absorbing any negative energy.")
+        furhat.say(
+            "I see that you're angry. While in this visualization, acknowledge your anger and visualize letting it "
+            "go. Picture your serene place absorbing any negative energy.")
         sleep(3)
         askForBreak()
 
     elif emotion == "happy":
-        furhat.say("Great to see you're feeling happy! In your visualization, amplify this happiness. Picture your serene place filled with joyful energy.")
+        furhat.say(
+            "Great to see you're feeling happy! In your visualization, amplify this happiness. Picture your serene "
+            "place filled with joyful energy.")
         sleep(3)
         askForBreak()
 
     elif emotion == "sad":
-        furhat.say("I notice that you're feeling sad. During this visualization, allow the peaceful setting to envelop you with comfort. Visualize releasing any sadness with each breath.")
+        furhat.say(
+            "I notice that you're feeling sad. During this visualization, allow the peaceful setting to envelop you "
+            "with comfort. Visualize releasing any sadness with each breath.")
         sleep(3)
         askForBreak()
 
     elif emotion == "calm":
-        furhat.say("I sense that you're feeling calm. Use this calmness to deepen your visualization experience. Picture your serene place with heightened clarity and tranquility.")
+        furhat.say(
+            "I sense that you're feeling calm. Use this calmness to deepen your visualization experience. Picture "
+            "your serene place with heightened clarity and tranquility.")
         sleep(3)
         askForBreak()
-    
+
     else:
-        furhat.say("I understand that emotions can be complex. Whatever you're feeling, allow gratitude to be a source of comfort and positivity.")
+        furhat.say(
+            "I understand that emotions can be complex. Whatever you're feeling, allow gratitude to be a source of "
+            "comfort and positivity.")
         sleep(3)
         askForBreak()
+
 
 # ----- Gratitude : Responses to each of the emotions : angry, happy, sad, calm/relaxed  -----
 def gratitudeEmotionResponse(furhat, emotion):
     if emotion == "angry":
-        furhat.say("I see that you're Angry. I understand that anger can be challenging. Acknowledge it and allow gratitude to bring a sense of peace and positivity.")
+        furhat.say(
+            "I see that you're Angry. I understand that anger can be challenging. Acknowledge it and allow gratitude "
+            "to bring a sense of peace and positivity.")
         sleep(3)
         return "happy"
-    
+
     elif emotion == "sad":
-        furhat.say("I see that you're Sad. Feeling sad is a natural part of life. Embrace gratitude to lift your spirits and find comfort in the positive aspects of your day.")
+        furhat.say(
+            "I see that you're Sad. Feeling sad is a natural part of life. Embrace gratitude to lift your spirits and "
+            "find comfort in the positive aspects of your day.")
         sleep(3)
         return "calm/relaxed"
-    
+
     elif emotion == "calm":
-        furhat.say("I see that you're calm. It's wonderful that you're feeling calm. Use this calmness to deepen your sense of gratitude and appreciation for the present moment.")
+        furhat.say(
+            "I see that you're calm. It's wonderful that you're feeling calm. Use this calmness to deepen your sense "
+            "of gratitude and appreciation for the present moment.")
         sleep(3)
         return "happy"
-    
+
     elif emotion == "happy":
-        furhat.say("I see that you're happy. That's Fantastic! Embrace the joy you're feeling. Let gratitude amplify this happiness and create a positive ripple effect in your day.")
+        furhat.say(
+            "I see that you're happy. That's Fantastic! Embrace the joy you're feeling. Let gratitude amplify this "
+            "happiness and create a positive ripple effect in your day.")
         sleep(3)
         return "calm/relaxed"
-    
+
     else:
-        furhat.say("I understand that emotions can be complex. Whatever you're feeling, allow gratitude to be a source of comfort and positivity.")
+        furhat.say(
+            "I understand that emotions can be complex. Whatever you're feeling, allow gratitude to be a source of "
+            "comfort and positivity.")
         sleep(3)
         return "something"
+
 
 # ----- Break : Function that triggers when asking for a break  -----
 def askForBreak(furhat):
     while True:
         furhat.say("Would you like to take a break?")
         result = furhat.listen()
-        
+
         if result.lower() == "yes":
             furhat.say("Great! let's take a 10 second break")
             furhat.gesture(name="CloseEyes")
@@ -122,13 +148,14 @@ def askForBreak(furhat):
             furhat.gesture(name="OpenEyes")
             furhat.say("Let's continue.")
             break  # Exit the loop if the user wants a break
-        
+
         elif result.lower() == "no":
             furhat.say("Great! Let's continue.")
             break  # Exit the loop if the user doesn't want a break
-       
+
         else:
             furhat.say("I'm sorry, I didn't get that")
+
 
 # ----- Conclusion Exercise  -----
 def conclusionExercise(furhat):
